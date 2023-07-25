a4c4b0369af21864520a46fc51c2c0dd08fe9dae
RollingInTheGitHub <140398567+RollingInTheGitHub@users.noreply.github.com>
2023-07-25 15:11:38 +0800

    Initial commit

---
+++ TestCreal/TestCreal.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+namespace CReal
+{
+    public class TestCreal : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            Debug.Log("Hello CReal.");
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+
+}
